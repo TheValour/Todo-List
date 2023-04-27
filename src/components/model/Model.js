f126cd75a0b5c611fe8f885650a12ab3d21e4feb
The_Valour <108991640+TheValour@users.noreply.github.com>
2023-04-27 18:54:01 +0000

    using CreatePortal method

--- src/components/model/Model.js
+++ src/components/model/Model.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import  ReactDOM  from 'react-dom'
 import style from './Model.module.css'
 
-export default function Model({setFlag}) {
+const ModelPortal = (props) => {
   return (
-    <div className={style.backdrop}>
-      <div className={`flex ${style.modal}`}>
-        <h1>Wrong Input</h1>
-        <button onClick={() => setFlag((pre) => !pre)}>Ok</button>
+      <div className={style.backdrop}>
+        <div className={`flex ${style.modal}`}>
+          <h1>Wrong Input</h1>
+          <button onClick={() => props.setFlag((pre) => !pre)}>Ok</button>
+        </div>
       </div>
-    </div>
+    )
+}
+
+function Model({setFlag}) {
+  return (
+    <>
+      { 
+        ReactDOM.createPortal(
+          <ModelPortal setFlag = {setFlag}/>, document.getElementById('model_backdrop')
+        )
+      }
+    </>
   )
 }
+export default  Model;
